e8868a576cf5a6266cd20c754efd644cfcb78d1b
abdalrhmanaboalsoud <abdalrhmanwaleed1999@gmail.com>
2024-03-12 02:10:09 +0300

    everything goes right now

--- src/components/TourDetails/TourDetails.js
+++ src/components/TourDetails/TourDetails.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 import toursData from "../../data/db.json";
-// import Dropdown from 'react-bootstrap/Dropdown';
+import "./TourDetails.css";
 
 function TourDetails() {
-    let { id } = useParams();
-    const city = toursData.find(city => city.id === parseInt(id));
+  const { id } = useParams();
+  const city = toursData.find((city) => city.id === id);
 
-    return (
+  return (
+    <div className="tour-details">  
+      {city ? (
         <div>
-            <h2>City Details</h2>
-            {city ? (
-                <div>
-                    <h3>Name: {city.name}</h3>
-                    <img id="imgID" src={city.image} alt="TourImage" />
-                    <p>Info: {city.info}</p>
-                </div>
-            ) : (
-                <div>
-                    <h3>City not found</h3>
-                </div>
-            )}
-            
+          <hr></hr>
+          <h1>{city.name}</h1>
+          <hr></hr>
+          <img id="imgID" src={city.image} alt="TourImage" />
+          <p>{city.info}</p>
         </div>
-    );
+      ) : (
+        <div className="city-not-found">
+          <h3>City not found</h3>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default TourDetails;
